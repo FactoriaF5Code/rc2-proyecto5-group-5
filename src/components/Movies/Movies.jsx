bf1b9a97bc4a4e7fe62b077b50aabec0da85c4c1
David-MarCle <martinezclementedavid@gmail.com>
2024-01-04 11:06:54 +0100

    Componente movies

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,46 @@
+import './Movies.css';
+import {useState,useEffect} from 'react';
+
+export const Movies = () => { 
+
+
+const [data, setData] = useState([]);
+const apiKey = "dd26df0c4c5b1b95d64c9c4568821204";
+
+useEffect(() => {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      setData(response.results);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}, []);
+
+return (
+  <div className="App">
+    <h1>Películas</h1>
+    <div className="movie">
+      {data.length === 0 ? (
+        <p>Cargando...</p>
+      ) : (
+        <ul>
+          {data.map((movie) => (
+            <li key={movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w300${movie.backdrop_path}`}
+                alt={movie.title}
+              />
+              <h3>{movie.title}</h3>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  </div>
+)
+}
--- src/components/data/Request.jsx
+++
@@ -1,7 +0,0 @@
-/* const API_KEY = "dd26df0c4c5b1b95d64c9c4568821204";
-
-const requests = {
-    fetchMovies: `/discover/movie?API_KEY=${API_KEY}&language=en-US`
-}
-
-export default requests; */
