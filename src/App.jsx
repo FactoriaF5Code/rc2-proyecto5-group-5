8b2301606251ca0a24b7dd075820d634517b9b2d
Johnny <jonathanbc2012@gmail.com>
2024-01-05 14:48:09 +0100

    header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css';
-import {Movies} from './components/Movies/Movies';
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Movies } from "./components/Movies/Movies";
 
 
-function App(){
-  return(
-<Movies/>
-  )
+function App() {
+  return (
+    <div>
+      <Header />
+      <Movies className="section1" />
+      <Movies className="section2" />
+      <Movies className="section3" />
+      <Movies className="section4" />
+      <Movies className="section5" />
+      <Movies className="section6" />
+      <Movies className="section7" />
+      <Movies className="section8" />
+    </div>
+  );
 }
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+export function Header() {
+  return (
+  <header>
+    <img src="../../assets/img/logoAsturFlix.png" alt="" />
+    <nav>
+        
+    </nav>
+    <div className="header__userLogo">
+
+    </div>
+  </header>);
+}
