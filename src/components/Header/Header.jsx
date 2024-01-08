6a8f576fbfb3cb858df845759c4d9684ca85eb1c
Johnny <jonathanbc2012@gmail.com>
2024-01-08 14:15:04 +0100

    header created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,32 @@
 import "./Header.css";
+import logo from "../../assets/img/logoAsturFlix.png";
+import userImg from "../../assets/img/userImg.png";
+import btnMore from "../../assets/img/btn-mas.png";
+
 export function Header() {
   return (
-  <header>
-    <img src="../../assets/img/logoAsturFlix.png" alt="" />
-    <nav>
-        
-    </nav>
-    <div className="header__userLogo">
-
-    </div>
-  </header>);
+    <header>
+      <img src={logo} alt="" className="logo left" />
+      <nav className="left">
+        <ul>
+          <a href="#">
+            <li>Series</li>
+          </a>
+          <a href="#">
+            <li>Películas</li>
+          </a>
+        </ul>
+      </nav>
+      <ul className="header__userLogo right">
+        <a href="">
+          <li>
+            <img src={userImg} alt="" />
+          </li>
+        </a>
+        <li id="header__userLogo-more">
+          <img src={btnMore} alt="" />
+        </li>
+      </ul>
+    </header>
+  );
 }
