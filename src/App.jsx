b0e26674d0c72b2347d4db254293b9eedf8e03f5
Johnny <jonathanbc2012@gmail.com>
2024-01-08 15:00:00 +0100

    component series created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Movies } from "./components/Movies/Movies";
+import { Series } from "./components/Series/Series";
+Series
 
 
 function App() {
   return (
     <div>
       <Header />
-      <Movies className="section1" />
-      <Movies className="section2" />
-      <Movies className="section3" />
-      <Movies className="section4" />
-      <Movies className="section5" />
-      <Movies className="section6" />
-      <Movies className="section7" />
-      <Movies className="section8" />
+      <Series className="section1" />
+      <Series className="section2" />
+      <Series className="section3" />
+      <Series className="section4" />
+      <Series className="section5" />
+      <Series className="section6" />
+      <Series className="section7" />
+      <Series className="section8" />
     </div>
   );
 }
---
+++ src/components/Series/Series.jsx
@@ -0,0 +1,41 @@
+import "./Series.css";
+import { useState, useEffect } from "react";
+
+export const Series = () => {
+  const [data, setData] = useState([]);
+  const apiKey = "dd26df0c4c5b1b95d64c9c4568821204";
+
+  useEffect(() => {
+    const url = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.results);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="serie">
+        {data.length === 0 ? (
+          <p>Cargando...</p>
+        ) : (
+          <ul>
+            {data.map((serie) => (
+              <li key={serie.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${serie.backdrop_path}`}
+                  alt={serie.title}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
